536833a8317e6faf26da18f87e939313986c03cd
zlase <1454596199@qq.com>
2023-06-24 19:32:59 +0800

    [feature][删除部分文件，按照课件步骤进行添加]

--- service/service-acl/src/main/java/com/atguigu/ssyx/acl/controller/IndexController.java
+++ service/service-acl/src/main/java/com/atguigu/ssyx/acl/controller/IndexController.java
@@ -7,39 +7,38 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Handler;
 
 @Api(tags = "登录接口")
 @RestController
 @RequestMapping("/admin/acl/index")
-//@CrossOrigin //跨域
+@CrossOrigin   //跨域
 public class IndexController {
 
-    //   http://localhost:8201/admin/acl/index/login
-    //1 login登录
+    //  http://localhost:8201/admin/acl/index/login
+    //  1 login登录
     @ApiOperation("登录")
     @PostMapping("login")
     public Result login() {
         //返回token值
-        Map<String,String> map = new HashMap<>();
-        map.put("token","token-admin");
+        Map<String, String> map = new HashMap<>();
+        map.put("token", "token-admin");
         return Result.ok(map);
     }
 
-//    url: '/admin/acl/index/info',
-//    method: 'get',
+    //  url: '/admin/acl/index/info',
+    //  method: 'get',
     //2 getInfo 获取信息
     @ApiOperation("获取信息")
     @GetMapping("info")
     public Result info() {
-        Map<String,String> map = new HashMap<>();
-        map.put("name","admin");
-        map.put("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "admin");
+        map.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
         return Result.ok(map);
     }
 
-//    url: '/admin/acl/index/logout',
-//    method: 'post'
+    //  url: '/admin/acl/index/logout',
+    //  method: 'post'
     //3 logout 退出
     @ApiOperation("退出")
     @PostMapping("logout")
